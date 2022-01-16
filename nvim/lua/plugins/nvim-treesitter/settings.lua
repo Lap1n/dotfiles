01c3f9ec05d801367a7928ca5c080c9b880eff45
Philippe Lelievre <philippewlelievre@gmail.com>
2022-01-15 22:59:56 -0500

    fixed rname

--- nvim/lua/plugins/nvim-treesitter/settings.lua
+++ nvim/lua/plugins/nvim-treesitter/settings.lua
@@ -28,7 +28,7 @@ require('nvim-treesitter.configs').setup({
     smart_rename = {
       enable = true,
       keymaps = {
-        smart_rename = "grr",
+        smart_rename = "<space>lr",
       },
     },
   },
