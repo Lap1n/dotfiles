64f3225fe2646835711ace373b3740f86ed06d44
Philippe Lelievre <philippewlelievre@gmail.com>
2023-01-14 18:30:58 -0500

    update

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -1,15 +1,16 @@
 -- See :help toggleterm-roadmap
+local opts = { buffer = 0 }
+local visual_mode_mapping = "<esc><esc>"
 require('toggleterm').setup({
 	--open_mapping = '<C-g>',
 	open_mapping = '<f1>',
 	direction = 'horizontal',
-	shade_terminals = true
+	shade_terminals = true,
 })
 
 -- if you only want these mappings for toggle term use term://*toggleterm#* instead
 function _G.set_terminal_keymaps()
-	local opts = { buffer = 0 }
-	vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+	vim.keymap.set('t', visual_mode_mapping, [[<C-\><C-n>]], opts)
 	--	vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
 	vim.keymap.set('t', '<C-w>h', [[<Cmd>wincmd h<CR>]], opts)
 	vim.keymap.set('t', '<C-w>j', [[<Cmd>wincmd j<CR>]], opts)
@@ -22,26 +23,31 @@ vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
 
 local Terminal = require('toggleterm.terminal').Terminal
 
-local lazygit = Terminal:new({
-	cmd = "lazygit",
-	dir = "git_dir",
-	direction = "float",
-	float_opts = {
-		border = "double",
-	},
-	-- function to run on opening the terminal
-	on_open = function(term)
-		vim.cmd("startinsert!")
-		vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
-	end,
-	-- function to run on closing the terminal
-	on_close = function(term)
-		vim.cmd("startinsert!")
-	end,
-})
+-- local lazygit = Terminal:new({
+-- 	cmd = "lazygit",
+-- 	dir = "git_dir",
+-- 	direction = "float",
+-- 	float_opts = {
+-- 		border = "double",
+-- 	},
+-- 	-- function to run on opening the terminal
+-- 	on_open = function(term)
+-- 		vim.cmd("startinsert!")
+-- 		vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
+-- 	end,
+-- 	-- function to run on closing the terminal
+-- 	on_close = function(term)
+-- 		vim.cmd("startinsert!")
+-- 	end,
+-- })
+local lazygit = Terminal:new({ cmd = "lazygit", hidden = true, direction = "float",
+	on_open = function()
+		--vim.api.nvim_del_keymap('n', '<Esc>')
+		vim.keymap.del('t', visual_mode_mapping, opts)
+	end })
 
-function _lazygit_toggle()
+function _Lazygit_toggle()
 	lazygit:toggle()
 end
 
-vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua _Lazygit_toggle()<CR>", { noremap = true, silent = true })
