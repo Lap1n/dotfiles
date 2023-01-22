e306a29cb947a0b58de6c9b43379e4fa6ab5a8e5
Philippe Lelievre <philippewlelievre@gmail.com>
2023-01-22 11:08:13 -0500

    update

--- nvim/lua/plugins/code.lua
+++ nvim/lua/plugins/code.lua
@@ -28,6 +28,22 @@ return {
       "hrsh7th/cmp-emoji",
       "hrsh7th/cmp-path",
       {
+        "rcarriga/cmp-dap",
+        config = function()
+          require("cmp").setup({
+            enabled = function()
+              return vim.api.nvim_buf_get_option(0, "buftype") ~= "prompt" or require("cmp_dap").is_dap_buffer()
+            end,
+          })
+
+          require("cmp").setup.filetype({ "dap-repl", "dapui_watches", "dapui_hover" }, {
+            sources = {
+              { name = "dap" },
+            },
+          })
+        end,
+      },
+      {
         "zbirenbaum/copilot-cmp",
 
         config = function()
