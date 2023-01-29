fa3318ac643dacc68b4aa6f564953258a16835cd
Philippe Lelievre <philippewlelievre@gmail.com>
2023-01-29 17:51:11 -0500

    asdf

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -2,7 +2,7 @@ return {
   {
     "nvim-neo-tree/neo-tree.nvim",
     opts = {
-      default_source = "git_status",
+      --  default_source = "git_status",
       use_libuv_file_watcher = true,
       filesystem = {
         follow_current_file = true,
