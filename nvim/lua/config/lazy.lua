b3f4a67b1664c40c756128008f513d33f83e0207
Philippe Lelievre <philippewlelievre@gmail.com>
2023-01-29 10:02:39 -0500

    update

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -13,7 +13,8 @@ require("lazy").setup({
     -- import/override with your plugins
     { import = "plugins" },
     -- import any extras modules here
-    -- { import = "lazyvim.plugins.extras.lang.typescript" },
+    --{ import = "lazyvim.plugins.extras.ui.mini-animate" },
+    { import = "lazyvim.plugins.extras.ui.mini-starter" },
     -- { import = "lazyvim.plugins.extras.lang.json" },
   },
   defaults = {
--- nvim/lua/plugins/code.lua
+++ nvim/lua/plugins/code.lua
@@ -111,115 +111,19 @@ return {
         end, { "i", "s" }),
       })
     end,
-
-    -- ---@param opts cmp.ConfigSchema
-    -- opts = function(_, opts)
-    --   -- local has_words_before = function()
-    --   --   unpack = unpack or table.unpack
-    --   --   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-    --   --   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-    --   -- end
-    --
-    --   local luasnip = require("luasnip")
-    --   local cmp = require("cmp")
-    --
-    --   local select_opts = { behavior = cmp.SelectBehavior.Select }
-    --   local has_words_before = function()
-    --     if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
-    --       return false
-    --     end
-    --     local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-    --     return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
-    --   end
-    --
-    --   opts.sources =
-    --     cmp.config.sources(vim.list_extend(opts.sources, { { name = "emoji" }, { name = "copilot", group_index = 2 } }))
-    --
-    --   opts.mapping = vim.tbl_extend("force", opts.mapping, {
-    --     -- ["<Up>"] = cmp.mapping.select_prev_item(select_opts),
-    --     -- ["<Down>"] = cmp.mapping.select_next_item(select_opts),
-    --     --
-    --     ["<C-k>"] = cmp.mapping.select_prev_item(select_opts),
-    --     ["<C-j>"] = cmp.mapping.select_next_item(select_opts),
-    --     ["<C-space>"] = cmp.mapping.complete({}),
-    --     ["<C-e>"] = cmp.mapping.complete({}),
-    --
-    --     -- ["<C-p>"] = cmp.mapping.select_prev_item(select_opts),
-    --     -- ["<C-n>"] = cmp.mapping.select_next_item(select_opts),
-    --     --
-    --     -- ["<C-u>"] = cmp.mapping.scroll_docs(-4),
-    --     -- ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    --     --
-    --     -- ["<C-e>"] = cmp.mapping.abort(),
-    --     -- ["<CR>"] = cmp.mapping.confirm({ select = false }),
-    --     --
-    --     -- ["<C-d>"] = cmp.mapping(function(fallback)
-    --     --   if luasnip.jumpable(1) then
-    --     --     luasnip.jump(1)
-    --     --   else
-    --     --     fallback()
-    --     --   end
-    --     -- end, { "i", "s" }),
-    --     --
-    --     -- ["<C-b>"] = cmp.mapping(function(fallback)
-    --     --   if luasnip.jumpable(-1) then
-    --     --     luasnip.jump(-1)
-    --     --   else
-    --     --     fallback()
-    --     --   end
-    --     -- end, { "i", "s" }),
-    --     ["<Tab>"] = cmp.mapping(function(fallback)
-    --       if cmp.visible() and has_words_before() then
-    --         cmp.select_next_item()
-    --       elseif luasnip.expand_or_jumpable() then
-    --         luasnip.expand_or_jump()
-    --       else
-    --         fallback()
-    --       end
-    --     end, { "i", "s" }),
-    --     ["<S-Tab>"] = cmp.mapping(function(fallback)
-    --       if cmp.visible() then
-    --         cmp.select_prev_item()
-    --       elseif luasnip.jumpable(-1) then
-    --         luasnip.jump(-1)
-    --       else
-    --         fallback()
-    --       end
-    --     end, { "i", "s" }),
-    --   })
-    -- end,
   },
   {
     "nvim-telescope/telescope.nvim",
-    -- keys = {
-    --   -- add a keymap to browse plugin files
-    --   -- stylua: ignore
-    --   {
-    --     "<leader>fp",
-    --     function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
-    --     desc = "Find Plugin File",
-    --   },
-    -- },
-    -- change some options
-    -- opts = {
-    --   defaults = {
-    --     layout_strategy = "horizontal",
-    --     layout_config = { prompt_position = "top" },
-    --     sorting_strategy = "ascending",
-    --     winblend = 0,
-    --   },
-    -- },
-
-    keys = function(_, keys)
-      return {
-        { "<leader>sf", "<cmd>Telescope find_files<cr>", desc = "Search files" },
-        { "<leader>ss", "<cmd>Telescope find_files<cr>", desc = "Search files" },
-        { "<leader>st", "<cmd>Telescope live_grep<cr>", desc = "Search text" },
-        { "<leader>sb", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Search current buffer" },
-        { "<leader>sh", "<cmd>Telescope oldfiles<cr>", desc = "Recent" },
-        { "<leader>bl", "<cmd>Telescope buffers<cr>", desc = "Switch buffers" },
-      }
-    end,
+    -- keys = function(_, keys)
+    --   return {
+    --     { "<leader>sf", "<cmd>Telescope find_files<cr>", desc = "Search files" },
+    --     { "<leader>ss", "<cmd>Telescope find_files<cr>", desc = "Search files" },
+    --     { "<leader>st", "<cmd>Telescope live_grep<cr>", desc = "Search text" },
+    --     { "<leader>sb", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Search current buffer" },
+    --     { "<leader>sh", "<cmd>Telescope oldfiles<cr>", desc = "Recent" },
+    --     { "<leader>bl", "<cmd>Telescope buffers<cr>", desc = "Switch buffers" },
+    --   }
+    -- end,
 
     opts = function()
       --require("plugins.dressing")
