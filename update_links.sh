507dc0d7ebeedee572f3f30cb12787dde831ca7e
Philippe Lelievre <philippewlelievre@gmail.com>
2024-05-19 20:32:46 -0400

    update

--- update_links.sh
+++ update_links.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/bash
 
+# If argument is passed --delete, delete the folder before creating the links
 for d in dot_config/*; do
 	config_path=~/.config/$(basename $d)
-	echo "Deleting $config_path"
-	sudo rm -r $config_path
+	if [ "$1" == "--delete" ]; then
+		echo "Deleting $config_path"
+		sudo rm -r $config_path
+	fi
 	echo "Adding $config_path"
-	sudo ln -s $(pwd)/$d $config_path
+	sudo ln -s $(pwd)/$d ~/.config
 done
